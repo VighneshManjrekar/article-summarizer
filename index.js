08dbcb61afa1ed0d074475ab184bb0fb8bd5fde0
Rohan Yadav <yrohan740@gmail.com>
2022-10-29 15:12:58 +0530

    Added Cors support

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 global.__rootDir = __dirname;
 const path = require("path");
+const cors = require('cors')
+
 require("dotenv").config({ path: path.join(__dirname, "configs", ".env") });
 
 const express = require("express");
@@ -9,7 +11,7 @@ const PORT = process.env.PORT || 7000;
 
 const mainRouter = require("./routes/main.route");
 const connectDb = require("./configs/db");
-
+app.use(cors());
 app.use(express.json());
 app.use(mainRouter);
 app.use((err, req, res, next) => {
